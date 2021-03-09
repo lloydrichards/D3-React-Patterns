0f731466fd5f321f1063ec68c73dad70c4022172
Snowpack Build <actions@github.com>
2021-03-09 23:25:44 +0000

    Commit from GitHub Actions (Snowpack to Github Pages)

--- docs/dist/App.js
+++ docs/dist/App.js
@@ -9,7 +9,7 @@ const RenderProps = lazy(() => import("./Patterns/RenderProps/index.js"));
 const HigherOrderComponent = lazy(() => import("./Patterns/HigherOrderComponent/index.js"));
 const App = () => {
   return /* @__PURE__ */ React.createElement(Router, {
-    basename: "/"
+    basename: "/D3-React-Patterns"
   }, /* @__PURE__ */ React.createElement(Suspense, {
     fallback: /* @__PURE__ */ React.createElement("div", null, "Loading...")
   }, /* @__PURE__ */ React.createElement(Switch, null, /* @__PURE__ */ React.createElement(Route, {
